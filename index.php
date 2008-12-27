6579111c24c5eb3e4e092774cf98b268092547ed
Indranil Dasgupta <indranild@gmail.com>
2008-12-27 20:31:17 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+	Itsy Bitsy Gallery
+	------------------
+	(c) 2008 Indranil Dasgupta.
+	Released under the MIT license.
+	http://www.opensource.org/licenses/mit-license.php
+*/
+
+
+
+?>
