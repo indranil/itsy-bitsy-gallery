ad685eac59987b8c965df14d446c3bc0cd227778
Indranil Dasgupta <indranild@gmail.com>
2008-12-27 21:23:01 +0530

    Started on it

--- index.php
+++ index.php
@@ -1,13 +1,35 @@
 <?php
 
-/*
-	Itsy Bitsy Gallery
-	------------------
-	(c) 2008 Indranil Dasgupta.
-	Released under the MIT license.
-	http://www.opensource.org/licenses/mit-license.php
-*/
+$ts = microtime(true);
+
+/**
+ * Itsy Bitsy Gallery
+ *
+ * @author Indranil Dasgupta
+ * @version 0.4.2
+ * @copyright Indranil Dasgupta, 27 December, 2008
+ * @package default
+ **/
+
+if(version_compare(PHP_VERSION,'5','<'))
+	die('Sorry, we require PHP 5+ to properly function!');
 
 
+// Let's see where we are.
+define('PATH', @realpath(dirname('__FILE__')));
+
+if($dir_handle = scandir(PATH)) {
+	echo '<pre>';
+	print_r($dir_handle);
+	echo '</pre>';
+	
+	
+	
+} else {
+	die('Invalid directory. Pain!');
+}
+
+echo '<br>';
+echo microtime(true) - $ts;
 
 ?>
