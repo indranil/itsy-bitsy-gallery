66a4defbd84b0369d02849501bc0e6e61301c680
Indranil Dasgupta <indranild@gmail.com>
2009-06-09 20:23:58 +0530

    Added readme.

--- index.php
+++ index.php
@@ -11,17 +11,32 @@ $ts = microtime(true);
  * @package default
  **/
 
-if(version_compare(PHP_VERSION,'5','<'))
-	die('Sorry, we require PHP 5+ to properly function!');
+if(version_compare(PHP_VERSION,'5','<')) die('Sorry, we require PHP 5+ to properly function!');
 
 
 // Let's see where we are.
-define('PATH', @realpath(dirname('__FILE__')));
+$path = @realpath(dirname('__FILE__'));
 
-if($dir_handle = scandir(PATH)) {
-	echo '<pre>';
-	print_r($dir_handle);
-	echo '</pre>';
+$types = array('image/bmp','image/gif','image/x-icon','image/jpeg','image/pjpeg','image/x-jps','image/x-portable-bitmap','image/x-pict','image/x-pcx','image/pict','image/png','image/tiff','image/x-tiff');
+
+$files = array();
+
+// We're scanning the directory now
+if($dir_handle = @scandir($path)) {
+	$i = 0;
+	foreach($dir_handle as $file) {
+		$flag = 0;
+		echo mime_content_type($file);
+		for($j=0;$j<count($types);$j++) {
+			if($file === $type[$j]) {
+				$flag = 1;
+			}
+		}
+		if($flag === 0) {
+			$files[$i] = $file;
+			$i++;
+		}
+	}
 	
 	
 	
