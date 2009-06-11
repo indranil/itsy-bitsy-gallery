ead346aef72a798f7f1a349e0506628798fac512
Indranil Dasgupta <indranild@gmail.com>
2009-06-11 14:36:51 +0530

    Styles put in, thumbnails scale. Now for individual images, and we release :)

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $ts = microtime(true);
 */
 
 $config['site_title']	=	'Itsy Bitsy Gallery';		// The title of the site.
-$config['heading']		=	'Gallery';					// The header of the page.
+$config['heading']		=	'Itsy Bitsy Gallery';		// The header of the page.
 $config['min_width']	=	'400';						// The minimum width for the thumbnails
 $config['min_height']	=	'400';						// The minimum height for the thumbnails
 $config['box_width']	=	'450';						// The width of the box containing the thumbnail
@@ -62,6 +62,23 @@ if($dir_handle = @scandir($path)) {
 			foreach($file_info as $key => $value) {
 				$files[$i][$key] = $value;
 			}
+			if($files[$i][0] >= $files[$i][1]) {
+				if($files[$i][0] > $config['min_width']) {
+					$files[$i]['thumb_width'] = $config['min_width'];
+					$files[$i]['thumb_height'] = $files[$i]['thumb_width'] * ($files[$i][1] / $files[$i][0]);
+				} else {
+					$files[$i]['thumb_width'] = $files[$i][0];
+					$files[$i]['thumb_height'] = $files[$i][1];
+				}
+			} else {
+				if($files[$i][1] > $config['min_height']) {
+					$files[$i]['thumb_height'] = $config['min_height'];
+					$files[$i]['thumb_width'] = $files[$i]['thumb_height'] * ($files[$i][0] / $files[$i][1]);
+				} else {
+					$files[$i]['thumb_height'] = $files[$i][1];
+					$files[$i]['thumb_width'] = $files[$i][0];
+				}
+			}
 			$i++;
 		}
 	}
@@ -76,39 +93,100 @@ if($dir_handle = @scandir($path)) {
 <html>
 <head>
 	<title><?php echo $config['site_title']; ?></title>
+	
+	<style type="text/css" media="screen">
+	<![CDATA[
+	
+	* {
+		margin: 0;
+		padding: 0;
+	}
+	
+	a {
+		color: #536661;
+		text-decoration: underline;
+		padding: 1px;
+	}
+	
+	a:hover {
+		color: #536661;
+		background-color: #afa;
+		text-decoration: none;
+	}
+	
+	a img {
+		border: none;
+	}
+	
+	body {
+		font-family: Helvetica, Arial, "Lucida Grande", Verdana, sans-serif;
+		text-align: center;
+		background-color: #A8AD90;
+		color: #36362C;
+	}
+	
+	.wrapper {
+		text-align: left;
+		width: 1000px;
+		margin: 0 auto;
+	}
+	
+	h1 {
+		font: normal 35px/40px Georgia, "Times New Roman", serif;
+		border-bottom: 1px dotted #536661;
+	}
+	
+	.imgcount {
+		font-style: italic;
+		font-size: 12px;
+		margin-top: -10px;
+		padding-bottom: 10px;
+	}
+	
+	.gallery {
+		list-style: none;
+	}
+	
+	.gallery li {
+		float: left;
+		width: 450px;
+		height: 450px;
+		text-align: center;
+		margin: 0 30px 30px 0;
+	}
+	
+	.gallery li img {
+		border: 2px solid green;
+	}
+	
+	.footer {
+		clear: both;
+		border-top: 1px dotted #536661;
+		padding-top: 4px;
+		font-size: 12px;
+		padding: 10px;
+	}
+	
+	]]>
+	</style>
+	
 </head>
 <body>
+<div class="wrapper">
 	<h1><?php echo $config['heading']; ?></h1>
-	<p><?php echo $num_files; ?> images contained within the folder.</p>
-	<ul>
+	<p class="imgcount"><?php echo $num_files; ?> images contained within the folder.</p>
+	<ul class="gallery">
 		<?php foreach($files as $imgfile) {
-			if($imgfile[0] >= $imgfile[1]) {
-				if($imgfile[0] > $config['min_width']) {
-					$width = $config['min_width'];
-					$height = $width * ($imgfile[1] / $imgfile[0]);
-				} else {
-					$width = $imgfile[0];
-					$height = $imgfile[1];
-				}
-			} else {
-				if($imgfile[1] > $config['min_height']) {
-					$height = $config['min_height'];
-					$width = $height * ($imgfile[0] / $imgfile[1]);
-				} else {
-					$height = $imgfile[1];
-					$width = $imgfile[0];
-				}
-			}
-			echo '<li><img src="' . $imgfile['name'] . '" width="' . $width . '" height="' . $height . '" alt="image" /></li>' . "\n";
+			echo '<li><img src="' . $imgfile['name'] . '" width="' . $imgfile['thumb_width'] . '" height="' . $imgfile['thumb_height'] . '" alt="image" /></li>' . "\n";
 		} ?>
 	</ul>
 	
-	<div class="footer">
+	<p class="footer">
 		Copyright &copy; <?php echo $config['copy_year'] . ', ' . $config['copy_holder']; ?>. All Rights Reserved. Powered by <a href="#">Itsy Bitsy Gallery</a>.
-	</div>
+	</p>
+</div>
 </body>
 </html>
 <?php
-echo '<br>';
-echo microtime(true) - $ts;
+/* that should be all. :) thanks for using Itsy Bitsy Gallery */
 ?>
