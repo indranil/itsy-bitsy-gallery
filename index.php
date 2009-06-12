1ecac7846b09c7155b9c38a0fe901ab56747aaba
unknown <Indranil@.(none)>
2009-06-12 12:13:08 +0530

    And now, hopefully, we have pagination. :)

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
-
-$ts = microtime(true);
-
 /**
  * Itsy Bitsy Gallery
  * ==================
  * @author Indranil Dasgupta
- * @version 0.4.2
+ * @version 0.5.0
  * @copyright Indranil Dasgupta, 2009
  * @package default
  **/
@@ -27,6 +24,7 @@ $config['box_width']	=	'450';						// The width of the box containing the thumbn
 $config['box_height']	=	'450';						// The height of the box containing the thumbnail
 $config['copy_holder']	=	'Photographer';				// The person whose name comes as copyright holder
 $config['copy_year']	=	'2009-2010';				// The years copyrighted for
+$config['thumb_page']	=	8;							// The number of thumbnails per page.
 
 
 /* End Configuration.
@@ -35,6 +33,13 @@ $config['copy_year']	=	'2009-2010';				// The years copyrighted for
 */
 
 if(version_compare(PHP_VERSION,'5','<')) die('Sorry, we require PHP 5+ to properly function!');
+
+if(isset($_GET['img']) && isset($_GET['pg'])) {
+	$_GET['pg'] = '';
+}
+
+// Minor precaution for any updaters out there. :P
+if(!isset($config['thumb_page'])) $config['thumb_page'] = 8;
 
 // Let's see where we are.
 $path = @realpath(dirname('__FILE__'));
@@ -195,6 +200,15 @@ if($dir_handle = @scandir($path)) {
 		text-align: center;
 	}
 	
+	.pagination {
+		text-align: center;
+		font-size: 12px;
+	}
+	
+	.pagination a:hover {
+		background: none !important;
+	}
+	
 	.footer {
 		clear: both;
 		border-top: 1px dotted #536661;
@@ -217,17 +231,56 @@ if($dir_handle = @scandir($path)) {
 		} ?>
 	</h1>
 	<?php if(isset($num_files) && $num_files != 0) { ?><p class="imgcount"><?php echo $num_files; ?> images contained within the folder.</p><?php } ?>
+	
 	<?php if(isset($_GET['img']) && $_GET['img'] != '') {
 		if(isset($msg)) echo $msg;
 		echo '<div class="main_img"><img src="' . $img_name . '" alt="image" /></div>'; 
 	} else { ?>
+	<?php /* Pagination is weirdly hard! Need to "class"-ify it by version 1.0 */ ?>
+	<?php if($num_files > $config['thumb_page']) {
+		if(isset($_GET['pg']) && $_GET['pg'] != '') {
+			$pg_num = (int) $_GET['pg'];
+			if($pg_num === 0) $pg_num = 1;
+			$pg_thumb_start = (($pg_num - 1) * $config['thumb_page']);
+			if(($pg_num * $config['thumb_page']) > $num_files) {
+				$pg_thumb_end = $num_files -1;
+			} else {
+				$pg_thumb_end = ($pg_thumb_start + $config['thumb_page']) -1;
+			}
+		} else {
+			$pg_num = 1;
+			$pg_thumb_start = 0;
+			$pg_thumb_end = $config['thumb_page'] - 1;
+		}
+	} else {
+		if($num_files === 0) {
+			$pg_num = 0;
+			$pg_thumb_start = 0;
+			$pg_thumb_end = -1;
+		} else {
+			$pg_num = 1;
+			$pg_thumb_start = 0;
+			$pg_thumb_end = $num_files-1;
+		}
+	} ?>
+	
 	<ul class="gallery">
-		<?php if(count($files) === 0) echo 'No images in folder!';
-		foreach($files as $imgfile) {
+		<?php if($num_files === 0) echo 'No images in folder!';
+		for($i=$pg_thumb_start;$i<=$pg_thumb_end;$i++) {
+			$imgfile = $files[$i];
 			echo '<li><a href="?img=' . $imgfile['name'] . '"><img src="' . $imgfile['name'] . '" width="' . $imgfile['thumb_width'] . '" height="' . $imgfile['thumb_height'] . '" alt="image" /></a></li>' . "\n";
 		} ?>
 	</ul>
+	<?php $num_pages = ceil($num_files / $config['thumb_page']); ?>
+	<?php if($pg_num != 0) { ?>
+	<p class="pagination">
+		<?php if($pg_num != 1) { ?><a href="<?php echo '?pg=' . ($pg_num - 1); ?>">&laquo; Previous</a><?php } ?>
+		Displaying page <?php echo $pg_num . ' of ' . $num_pages; ?>
+		<?php if($pg_num < $num_pages) { ?><a href="<?php echo '?pg=' . ($pg_num + 1); ?>">Next &raquo;</a><?php } ?>
+	</p>
 	<?php } ?>
+	
+	<?php } // see line #226 ?>
 	
 	<p class="footer">
 		Copyright &copy; <?php echo $config['copy_year'] . ', ' . $config['copy_holder']; ?>. All Rights Reserved. Powered by <a href="http://dawnstudios.com/gallery/">Itsy Bitsy Gallery</a>.
